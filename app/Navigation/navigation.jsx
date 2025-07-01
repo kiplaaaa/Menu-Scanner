941b43e1c1d98950947addd3e568c1773ae7b585
User <giddtkipscheruyot@gmail.com>
2025-07-01 22:42:32 +0300

    stating the App

---
+++ app/Navigation/navigation.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+
+    const stack = createNativeStackNavigator();
+    const Tab = createBottomTabNavigator();
+
+function MainNavigation(){
+    return(
+
+    )
+}
